307394edbebdba358b75dc3e3ae56afe00c9ae8f
Aske Lysgaard <askelysgaard@gmail.com>
2024-03-11 14:58:12 +0100

    Add project files.

---
+++ WeatherApp/Models/JSONWeather.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+
+namespace WeatherApp.Models;
+
+public class JSONWeather
+{
+    [JsonProperty("temp")]
+    public double Temp;
+
+    [JsonProperty("feels_like")]
+    public double FeelsLike;
+
+    [JsonProperty("temp_min")]
+    public double TempMin;
+
+    [JsonProperty("temp_max")]
+    public double TempMax;
+
+    [JsonProperty("pressure")]
+    public int Pressure;
+
+    [JsonProperty("humidity")]
+    public int Humidity;
+}
---
+++ WeatherApp/Models/Weather.cs
@@ -0,0 +1,9 @@
+﻿using Newtonsoft.Json;
+
+namespace WeatherApp.Models;
+
+public class Weather
+{
+    [JsonProperty("list")]
+    public List<WeeklyForecast>? WeatherList { get; set; }
+}
